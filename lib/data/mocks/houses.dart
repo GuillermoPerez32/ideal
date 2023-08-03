98b8424fc0fd97d33c02f0da6dce943f93a8d097
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-08-03 00:43:45 -0400

    model and mock

---
+++ lib/data/mocks/houses.dart
@@ -0,0 +1,36 @@
+import 'package:ideal/data/models/house.dart';
+
+final List<House> mockHouses = [
+  House(
+    name: 'habana',
+    city: 'arroyo',
+    image:
+        'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    price: 25,
+    state: 'habana',
+  ),
+  House(
+    name: 'habana',
+    city: 'arroyo',
+    image:
+        'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    price: 25,
+    state: 'habana',
+  ),
+  House(
+    name: 'habana',
+    city: 'arroyo',
+    image:
+        'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    price: 25,
+    state: 'habana',
+  ),
+  House(
+    name: 'habana',
+    city: 'arroyo',
+    image:
+        'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    price: 25,
+    state: 'habana',
+  ),
+];
---
+++ lib/data/models/house.dart
@@ -0,0 +1,15 @@
+class House {
+  final String name;
+  final String state;
+  final String city;
+  final int price;
+  final String image;
+
+  House({
+    required this.state,
+    required this.city,
+    required this.price,
+    required this.image,
+    required this.name,
+  });
+}
